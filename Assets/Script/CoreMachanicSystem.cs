a959afb902496e1b27c950b62eb5ff69aa5cf2c2
EskiNott <heisesd@gmail.com>
2022-09-25 11:05:23 +0800

    add new Machanic to drag item

--- Assets/Script/CoreMachanicSystem.cs
+++ Assets/Script/CoreMachanicSystem.cs
@@ -5,19 +5,58 @@ using UnityEngine;
 
 public class CoreMachanicSystem : MonoSingleton<CoreMachanicSystem>
 {
-    public float PushForce = 100.0f;
+    public float PushForce;
     public LayerMask layerMask;
+
+    [SerializeField] private bool Dragging = false;
+    private Vector3 StartDragging;
+    private Vector3 StopDragging;
+    private RaycastHit hit;
+    private RaycastHit hitSecond;
+
     private void Update()
     {
         if (!UIManager.Instance.GetUISituation())
         {
-            ClickToPushMachanic();
+            //ClickToPushMachanic();
+            DragToBounceMachanic();
         }
     }
 
-    public void ClickToPushMachanic()
+    private void DragToBounceMachanic()
     {
-        RaycastHit hit = RaycastPhysical();
+        if (Input.GetMouseButtonDown(0))
+        {
+            hit = RaycastPhysical();
+            if (hit.collider.CompareTag("Item"))
+            {
+                Dragging = true;
+                StartDragging = hit.point;
+            }
+        }
+        else if (Input.GetMouseButtonUp(0) && Dragging && hit.collider.GetComponent<Rigidbody>() != null) 
+        {
+            Dragging = false;
+            hitSecond = RaycastPhysical();
+            StopDragging = new Vector3(hitSecond.point.x, StartDragging.y, hitSecond.point.z);
+            Push(hit.collider.GetComponent<Rigidbody>(), StartDragging - StopDragging, (StartDragging - StopDragging).magnitude * 300);
+        }else if (Input.GetMouseButton(0) && Dragging)
+        {
+/*            RaycastHit hitSecond = RaycastPhysical();
+            Vector3 MouseWorld = new Vector3(hitSecond.point.x, StartDragging.y, hitSecond.point.z);
+            Debug.DrawLine(StartDragging, MouseWorld, Color.red);*/
+        }
+    }
+
+    private void Push(Rigidbody rb, Vector3 Direction, float PushForce)
+    {
+        GameManager.Instance.IncreaseMerit(GameManager.Instance.GetMeritStrength());
+        rb.AddForce(Direction.normalized * PushForce);
+    }
+
+    private void ClickToPushMachanic()
+    {
+        hit = RaycastPhysical();
         if (hit.collider != null)
         {
             GameObject hitObject = hit.collider.gameObject;
@@ -35,8 +74,7 @@ public class CoreMachanicSystem : MonoSingleton<CoreMachanicSystem>
                     if (Input.GetMouseButtonDown(0))
                     {
                         Debug.Log("Push!");
-                        GameManager.Instance.IncreaseMerit(GameManager.Instance.GetMeritStrength());
-                        rb.AddForce(Direction * PushForce);
+                        Push(rb, Direction, PushForce);
                     }
                 }
             }
